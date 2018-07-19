6d47ca4a82db2d9a8fb55f5e0e2b26e8ac4a4680
zms00@139.com <zms00@139.com>
2018-07-19 13:43:50 +0800

    123

--- src/main/java/com/java/zms/ZmsApplication.java
+++ src/main/java/com/java/zms/ZmsApplication.java
@@ -1,12 +1,17 @@
 package com.java.zms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZmsApplication {
+   static Logger log  = LoggerFactory.getLogger(ZmsApplication.class);
 
     public static void main(String[] args) {
+
         SpringApplication.run(ZmsApplication.class, args);
     }
 }
